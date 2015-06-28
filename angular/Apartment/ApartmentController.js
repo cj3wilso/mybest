fbd448c5e4580d56f19681088a4b2097d16e9669
Christine Wilson <cj3wilso@gmail.com>
2015-06-28 19:06:39 -0400

    added contact button to all pages - but need a better onegit add -A

--- angular/Apartment/ApartmentController.js
+++ angular/Apartment/ApartmentController.js
@@ -1,12 +1,13 @@
 'use strict';
 
 define(['app'], function (app) {
-    app.register.controller('ApartmentController', ['$scope', '$http', '$route', '$routeParams', 'hash', 'apartmentService',
-     function ($scope, $http, $route, $routeParams, hash, apartmentService) {
+    app.register.controller('ApartmentController', ['$scope', '$http', '$route', '$rootScope', '$routeParams', 'hash', 'apartmentService',
+     function ($scope, $http, $route, $rootScope, $routeParams, hash, apartmentService) {
 		apartmentService.getAptPage($routeParams.propid)
             .then(function (data) {
                 $scope.hash = hash;
 				$scope.results = data;	
+				$rootScope.propid = $scope.results.records["id_pg"];
 		}, function (error) {
 			$window.alert('Error getting apartment: ' + error.message);
 		});
